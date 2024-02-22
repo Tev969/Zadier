948f48af45c6f1128e0774e4aa4fd8c2b36f6286
Tev969 <freezuka14@gmail.com>
2024-02-22 13:25:05 +0100

    first commit

--- src/commands/russian.ts
+++ src/commands/russian.ts
@@ -1,5 +1,5 @@
 import { Command } from '@commands';
-import {  SlashCommandBuilder , CommandInteraction ,  ActionRowBuilder , ButtonBuilder , ButtonStyle , Client , Message } from 'discord.js';
+import {  SlashCommandBuilder , CommandInteraction , SelectMenuBuilder ,  ActionRowBuilder , ButtonBuilder , ButtonStyle} from 'discord.js';
 
 /**
  * @command     - russianRoulette 
@@ -13,8 +13,7 @@ export const RUSSIAN: Command = {
         .setDescription("Try a russian roulette and get mute if you lose ? Do you have balls ? @_@ "),
 
     async execute(interaction: CommandInteraction) {
-     const { client , user } = interaction
-
+     const { user } = interaction
 
      const accept = new ButtonBuilder()
      .setCustomId('confirm')
@@ -25,7 +24,35 @@ export const RUSSIAN: Command = {
      .setCustomId('decline')
      .setLabel('Bah alors ?')
      .setStyle(ButtonStyle.Secondary)
-    }
 
 
+     const filter = (i) => i.customId === "confirm" && i.userId === interaction.user.id
+     const collector = interaction.channel.createMessageComponentCollector({filter , time:20000})
+
+     //faire 2 collector quand je clique sur & des 2 button je recois un messag et supprime les butt
+
+     
+     const numbers = [1,2,3,4,5,6]
+     const numbersOption = numbers.map( num => ({
+          label: num.toString(),
+          value: num.toString(),
+     }));
+
+     const selectMenu = new SelectMenuBuilder()
+     .setCustomId('selectNumber')
+     .setPlaceholder('Sélectionnez un nombre')
+     .addOptions(numbersOption);
+
+     
+
+     const row = new ActionRowBuilder()
+     .addComponents(accept , decline)
+     
+     await interaction.reply({
+
+          content: `hello  for ${user}`,
+          components:[row],
+          ephemeral: false
+     });
+    },
 };

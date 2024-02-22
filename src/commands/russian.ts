07e3d9b846dbec0fc6af097e9bb646f662419166
Tev969 <freezuka14@gmail.com>
2024-02-22 15:49:36 +0100

    resolve bug

--- src/commands/russian.ts
+++ src/commands/russian.ts
@@ -1,58 +1,68 @@
 import { Command } from '@commands';
-import {  SlashCommandBuilder , CommandInteraction , SelectMenuBuilder ,  ActionRowBuilder , ButtonBuilder , ButtonStyle} from 'discord.js';
-
-/**
- * @command     - russianRoulette 
- * @description - Amuser la galerie tel un tocard !
- * @permission  - None
- */
+import {
+    SlashCommandBuilder,
+    CommandInteraction,
+    StringSelectMenuBuilder,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ComponentType,
+    ButtonInteraction,
+} from 'discord.js';
 
 export const RUSSIAN: Command = {
     data: new SlashCommandBuilder()
-        .setName("russianroulette")
-        .setDescription("Try a russian roulette and get mute if you lose ? Do you have balls ? @_@ "),
+        .setName('russianroulette')
+        .setDescription('Try a russian roulette and get mute if you lose ? Do you have balls ? @_@ '),
 
     async execute(interaction: CommandInteraction) {
-     const { user } = interaction
+        const { user } = interaction;
 
-     const accept = new ButtonBuilder()
-     .setCustomId('confirm')
-     .setLabel('Prendre le risque.')
-     .setStyle(ButtonStyle.Danger)
+        const accept = new ButtonBuilder()
+            .setCustomId('confirm')
+            .setLabel('Prendre le risque.')
+            .setStyle(ButtonStyle.Danger);
 
-     const decline = new ButtonBuilder()
-     .setCustomId('decline')
-     .setLabel('Bah alors ?')
-     .setStyle(ButtonStyle.Secondary)
+        const decline = new ButtonBuilder()
+            .setCustomId('decline')
+            .setLabel('Bah alors ?')
+            .setStyle(ButtonStyle.Secondary);
 
+        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(accept, decline);
 
-     const filter = (i) => i.customId === "confirm" && i.userId === interaction.user.id
-     const collector = interaction.channel.createMessageComponentCollector({filter , time:20000})
+        await interaction.reply({
+            content: `hello  for ${user}`,
+            components: [row],
+            ephemeral: false,
+        });
 
-     //faire 2 collector quand je clique sur & des 2 button je recois un messag et supprime les butt
+        // Filtre pour collecter les interactions du bouton "confirm" émis par l'utilisateur
+        const filter = (i:ButtonInteraction) => i.customId === 'confirm' && i.user.id === interaction.user.id;
+        const collector = interaction.channel.createMessageComponentCollector({ componentType: ComponentType.Button , max: 1, filter, time: 20000 });
+        // On collecte tout les buttons , on les envoie à filter , si c'est true on envoie a collector.on , si false poubelle
 
-     
-     const numbers = [1,2,3,4,5,6]
-     const numbersOption = numbers.map( num => ({
-          label: num.toString(),
-          value: num.toString(),
-     }));
+        collector.on('collect', () => {
+            // Faire quelque chose lorsque le bouton "confirm" est cliqué
+        });
 
-     const selectMenu = new SelectMenuBuilder()
-     .setCustomId('selectNumber')
-     .setPlaceholder('Sélectionnez un nombre')
-     .addOptions(numbersOption);
+        // Création du menu déroulant avec les options de chiffres
+        const numbers = [1, 2, 3, 4, 5, 6];
+        const numberOptions = numbers.map((num) => ({
+            label: num.toString(),
+            value: num.toString(),
+        }));
 
-     
+        const selectMenu = new StringSelectMenuBuilder()
+            .setCustomId('menu')
+            .setPlaceholder('Prend un chiffre')
+            .setMaxValues(6)
+            .addOptions(numberOptions);
 
-     const row = new ActionRowBuilder()
-     .addComponents(accept , decline)
-     
-     await interaction.reply({
+        const numberRow = new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(selectMenu);
 
-          content: `hello  for ${user}`,
-          components:[row],
-          ephemeral: false
-     });
+        interaction.followUp({
+            content: 'Please select a number:',
+            components: [numberRow],
+        });
     },
 };
